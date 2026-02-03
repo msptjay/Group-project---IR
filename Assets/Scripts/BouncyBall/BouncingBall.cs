d3e537f66d85ee64a6cebbc879cce63ce619b7c6
Oshifudge <lucasadams812@gmail.com>
2026-02-03 18:23:32 +0000

    Ball bouncing
    Ball now bounces and has an "owner" for scoring purposes later !!

--- Assets/Scripts/BouncyBall/BouncingBall.cs
+++ Assets/Scripts/BouncyBall/BouncingBall.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class BouncingBall : MonoBehaviour
 {
-
+    private int ownedBy;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,9 +28,21 @@ public class BouncingBall : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Bounce!");
+            
             //rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
             rb.AddForce(new Vector2(Random.Range(-5f, 5f), 1 * 10f), ForceMode2D.Impulse);
+
+            if (collision.gameObject.name == "Player")
+            {
+                Debug.Log("collided with" + collision.gameObject.name);
+                ownedBy = 1;
+                sr.material.SetColor("_Color", Color.green);
+            }
+            if (collision.gameObject.name == "Player2")
+            {
+                ownedBy = 2;
+                sr.material.SetColor("_Color", Color.cornflowerBlue);
+            }
         }
     }
 }
