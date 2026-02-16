fed90dc8b496a3ff5ed8887d29e1f50b580ffe9a
Miles Jay <Mj.dev2867@proton.me>
2026-02-16 20:15:32 +0000

    Watch out progress
    Added a fair bit of code into the collumn manager to setup the system where the player selects a place before the end of the round then being locked in.

--- Assets/Scripts/WatchOut!/ColumnManager.cs
+++ Assets/Scripts/WatchOut!/ColumnManager.cs
@@ -4,34 +4,74 @@ using TMPro;
 
 public class ColumnManager : MonoBehaviour
 {
-    public Transform[] columns;
-    public GameObject branchPrefab;
-    public float timerCountdown = 5f;
-    public TextMeshProUGUI countdown;
-    /*
+    [Header("World stuff")]
+    [SerializeField] private Transform[] columns;
+    [SerializeField] private Collider2D[] playerSlots;
+    [SerializeField] private GameObject branchPrefab;
+    [SerializeField] private TextMeshProUGUI countdown;
+//    [SerializeField] private TextMeshProUGUI playerHealth1;
+   // [SerializeField] private TextMeshProUGUI playerHealth2;
+   [SerializeField] private float timerCountdown = 5f;
+    [SerializeField] private PlayerHealth player;
+    [SerializeField] private bool roundActive = false;
+    [SerializeField] private bool inPlace = false;
+    [SerializeField] private int ColumnPlace = -1;
+    
     private void Start()
     {
-        SpawnBranch();
-    } */
+        //StartRound();
+        
+    }
 
     private void Update()
     {
-        
-            timerCountdown -= Time.deltaTime;
-            countdown.text = "Timer : "  + timerCountdown;
-            if (timerCountdown <= 0)
-            {
-                SpawnBranch();
-                timerCountdown = 10f;
-            }
-            
+        timerCountdown -= Time.deltaTime;
+        countdown.text = "Timer : " + timerCountdown;
+        if (timerCountdown <= 0)
+        {
+            SpawnBranch();
+            timerCountdown = 10f;
+        }
+    }
+
+    public void StartRound()
+    {
+        roundActive = true;
+
+        if (ColumnPlace == -1)
+        {
+            Debug.Log("Player did not choose a column!");
+            return;
+        }
+
+    }
+
+    private void LockedInSpot(int column)
+    {
+        if (!roundActive)
+        {
+            ColumnPlace = column;
+        }
     }
     
-    public void SpawnBranch()
+    private void SpawnBranch()
     {
         int randomColumn = Random.Range(0, columns.Length);
         Instantiate(branchPrefab,columns[randomColumn].position,Quaternion.identity);
         Debug.Log("Spawned branch");
+    }
 
+    private void LockPlayerCollumn()
+    {
+        if (timerCountdown <= 0f && gameObject.CompareTag("Player"))
+        {
+            inPlace = true;
+            Debug.Log("You have cooked!");
+        }
+        else
+        {
+            inPlace = false;
+            Debug.Log("You haven't made it in time :(");
+        }
     }
 }
