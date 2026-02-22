eaf21d677d8fa4165b1507af507e460dd6aa4b0b
Oshifudge <lucasadams812@gmail.com>
2026-02-22 00:19:56 +0000

    Berry Plinko
    Fruit now tracks the correct basket it falls into (SCORE COMING SOON!!!!)

--- Assets/Scripts/PlinkoGame/PlinkoFruit.cs
+++ Assets/Scripts/PlinkoGame/PlinkoFruit.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlinkoFruit : MonoBehaviour
 {
+    [SerializeField] int FruitType; // 0 = Blueberry 1 = strawberry 2 = cranberry 3 = blackberry
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,49 @@ public class PlinkoFruit : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BluB Box"))
+        {
+            if(FruitType == 0)
+            {
+                Debug.Log("BLUEBERRY CORREC T!!!!!");
+                Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+        if(collision.gameObject.CompareTag("StrB Box"))
+        {
+            if (FruitType == 1)
+            {
+                Debug.Log("STRAWBERRY CORRECT!!!!!");
+                Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("CraB Box"))
+        {
+            if (FruitType == 2)
+            {
+                Debug.Log("CRANBERRY CORRECT!!!!!");
+                Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+        if(collision.gameObject.CompareTag("BlaB Box"))
+        {
+            if (FruitType == 3)
+            {
+                Debug.Log("BLACKBERRY CORRECT!!!!!");
+                Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
     }
 }
