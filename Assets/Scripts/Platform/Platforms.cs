86698979ae71ad180a81d4fdf42a7ade4640c9fd
Oshifudge <lucasadams812@gmail.com>
2026-02-26 16:46:04 +0000

    Counting spawn loop
    It spawns many many red fruits on loop, how fun!

--- Assets/Scripts/Platform/Platforms.cs
+++ Assets/Scripts/Platform/Platforms.cs
@@ -1,18 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class Platforms : MonoBehaviour
 {
-[SerializeField] private float timeCountdown;
+    [SerializeField] private float timeCountdown;
 
 
-public void Start()
-{
-    
-}
+    public void Start()
+    {
+        //I JUST HAD TO ADD THIS BECAUSE IT WOULDNT COMPILE
+        StartCoroutine(Countdown());
 
-IEnumerator Countdown()
-{
-    
-}
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(timeCountdown);
+    }
 
 }
