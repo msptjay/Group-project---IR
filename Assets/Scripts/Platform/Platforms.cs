a444f8bb35ae475683eed780fc890107eda134a2
Miles Jay <mjay.2867@proton.me>
2026-04-25 22:36:15 +0100

    Platform drop timers
    added timers to the platforms

--- Assets/Scripts/Platform/Platforms.cs
+++ Assets/Scripts/Platform/Platforms.cs
@@ -18,9 +18,9 @@ public class Platforms : MonoBehaviour
     
     [SerializeField] private float mainTimer;
     [SerializeField] private PlayerHealth player;
-   // [Header("Texts")] [SerializeField] private TextMeshProUGUI mainPlatforms;
-    
-   // [SerializeField] private TextMeshProUGUI platform1;
+   [Header("Texts")] [SerializeField] private TextMeshProUGUI mainPlatforms;
+
+    [SerializeField] private TextMeshProUGUI platform1;
 
     public void Start()
     {
@@ -87,7 +87,7 @@ public class Platforms : MonoBehaviour
             
             while (timer > 0)
             {
-               //platform1.text = "Timer: " + Mathf.FloorToInt(timer);
+               platform1.text = "Timer: " + Mathf.FloorToInt(timer);
                 timer -= Time.deltaTime;
                 yield return null;
             }
@@ -96,7 +96,7 @@ public class Platforms : MonoBehaviour
             Destroy(startingPlatform);
             while (timer1 > 0)
             {
-               // mainPlatforms.text = "Timer: " + Mathf.FloorToInt(timer1);
+               mainPlatforms.text = "Timer: " + Mathf.FloorToInt(timer1);
                 timer1 -= Time.deltaTime;
                 if (timer1 <= 0)
                 {
